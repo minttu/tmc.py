7e02e451f233a4379e3dd34c5254ecfc051a8119
Markus Holmström <markus.holmstrom2@gmail.com>
2014-08-12 23:01:08 +0300

    cleanup: API code refactoring, indentation fixes, etc.

--- tmc/api.py
+++ tmc/api.py
@@ -1,11 +1,11 @@
-import requests
+from requests import request
+from requests.exceptions import RequestException
+from functools import partial
 
 from tmc.errors import APIError
 from tmc.models import Config
 
-
 # from tmc.version import __version__
-
 
 class API:
 
@@ -26,6 +26,12 @@ class API:
         self.params = {
             "api_version": self.api_version
         }
+
+        # Essentially the same as requests.get and post
+        # but uses __do_request as a single point of entry to
+        # requests library
+        self.get = partial(self.__do_request, "GET")
+        self.post = partial(self.__do_request, "POST")
 
     def __ensure_configured(f):
         ''' 
@@ -60,26 +66,13 @@ class API:
 
     @__ensure_configured
     def make_request(self, slug, timeout=10):
-        req = requests.get("{0}{1}".format(self.server_url, slug),
-                           headers=self.auth_header,
-                           params=self.params,
-                           timeout=timeout)
-        if req is None:
-            raise APIError("Request is none!")
-        return self.get_json(req)
+        url = "{0}{1}".format(self.server_url, slug)
+        resp = self.get(url,
+                   headers=self.auth_header,
+                   params=self.params,
+                   timeout=timeout)
 
-    def get_json(self, req):
-        json = None
-        try:
-            json = req.json()
-        except ValueError:
-            if "500" in req.text:
-                raise APIError("TMC Server encountered a internal error.")
-            else:
-                raise APIError("TMC Server did not send valid JSON.")
-        if "error" in json:
-            raise APIError(json["error"])
-        return json
+        return self.__to_json(resp)
 
     def get_courses(self):
         return self.make_request("courses.json")["courses"]
@@ -93,25 +86,71 @@ class API:
 
     @__ensure_configured
     def get_zip_stream(self, exercise):
-        return requests.get("{0}exercises/{1}.zip".format(self.server_url,
-                                                          exercise.tid),
-                            stream=True,
-                            headers=self.auth_header,
-                            params=self.params)
+        tmpl = "{0}exercises/{1}.zip" 
+        url = tmpl.format(self.server_url, exercise.tid)
+        return self.get(url,
+                   stream=True,
+                   headers=self.auth_header,
+                   params=self.params)
 
     @__ensure_configured
     def send_zip(self, exercise, file, params):
-        return self.get_json(
-            requests.post(
-                "{0}exercises/{1}/submissions.json".format(
-                    self.server_url, exercise.tid),
-                headers=self.auth_header,
-                data={"api_version": self.api_version, "commit": "Submit"},
-                params=params,
-                files={"submission[file]": ('submission.zip', file)}))
+        tmpl = "{0}exercises/{1}/submissions.json"
+        url = tmpl.format(self.server_url, exercise.tid)
+        resp = self.post(
+            url,
+            headers=self.auth_header,
+            params=params,
+            files={
+                "submission[file]": ('submission.zip', file)
+            },
+            data={
+                "api_version": self.api_version, 
+                "commit": "Submit"
+            }
+        )
+        return self.__to_json(resp)
 
     def get_submission(self, id):
         req = self.make_request("submissions/{0}.json".format(id))
         if req["status"] == "processing":
             return None
         return req
+
+    def __do_request(self, method, url, **kwargs):
+        ''' 
+            Does HTTP using Requests.
+            Prevents RequestExceptions from propagating 
+        '''
+        #
+        # request() can raise connectivity related exceptions.
+        # raise_for_status raises an exception ONLY if the response 
+        # status_code is "not-OK" i.e 4XX, 5XX..
+        #
+        # All of these inherit from RequestException
+        #
+        try:
+            resp = request(method, url, **kwargs)
+            resp.raise_for_status()
+        except RequestException as e:
+            reason = "HTTP {0} request to {1} failed: {2}"
+            raise APIError(reason.format(method, url, repr(e)))
+
+        return resp
+
+    def __to_json(self, resp):
+        '''
+            Extract json from a response. 
+            Assumes response is valid otherwise.
+            Internal use only.
+        '''
+        try:
+            json = resp.json()
+        except ValueError as e:
+            reason = "TMC Server did not send valid JSON: {0}"
+            raise APIError(reason.format(repr(e)))
+
+        if "error" in json:
+            raise APIError("JSON error: {0}".format(json["error"]))
+        return json
+
